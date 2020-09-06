f5b8c6e24e30408ccedd8f715736ea073d54bcc6
Sarah Razanatsoa <sarah.raz@onja.org>
2020-09-06 15:00:48 +0300

    added the full script

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ const displayList = data => {
 			(person, index) => `
     <tr data-id="${person.id}" class="${index % 2 ? 'even' : ''}">
         <td><img src="${person.picture}" alt="${person.firstName + ' ' + person.lastName}"/></td>
-        <td>${person.lastName}</td>
-        <td>${person.firstName}</td>
-        <td>${person.jobTitle}</td>
-        <td>${person.jobArea}</td>
-        <td>${person.phone}</td>
+        <td class="last-name">${person.lastName}</td>
+        <td class="first-name">${person.firstName}</td>
+        <td class="job-title">${person.jobTitle}</td>
+        <td class="job-area">${person.jobArea}</td>
+        <td class="phone">${person.phone}</td>
         <td>
             <button class="edit">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="pencil w-6 h-6"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
@@ -39,20 +39,145 @@ const displayList = data => {
 		.join('');
 };
 
-const editPartner = () => {
-	// code edit function here
-};
+// handle the clicks
+const handleClicks = (e) => {
+	const button = e.target;
+	if(button.closest('button.edit')) {
+		const parent = button.closest('tr');
+		const id = parent.dataset.id;
+		editPartnerPopup(id);
+	}
+	if(button.closest('button.delete')) {
+		const parent = button.closest('tr');
+		const id = parent.dataset.id;
+		console.log('Delete');
+		deletePartner(id);
+	}
 
-const editPartnerPopup = () => {
+}
+
+// destroy the popup after submit or cancel
+async function destroyPopup(popup) {
+    popup.classList.remove('open');
+    //remove the popup from the DOM
+    popup.remove();
+    //remove from the javascript memory
+    popup = null;
+}
+
+// const editPartner = (id) => {
+// 	// code edit function here
+// 	let personToEdit = persons.find(person => person.id === id);
+// 	console.log(personToEdit);
+// 	editPartnerPopup(personToEdit, id);
+// };
+
+const editPartnerPopup = (id) => {
+	let personToEdit = persons.find(person => person.id === id);
 	// create edit popup here
+	return new Promise(async function(resolve) {
+		const formPopup = document.createElement('form');
+		formPopup.classList.add('popup');
+        formPopup.insertAdjacentHTML("afterbegin", `
+		<div>
+		<fieldset>
+            <label>Last name</label>
+            <input type="text" name="lastName" value="${personToEdit.lastName}">
+		</fieldset>
+		<fieldset>
+            <label>First name</label>
+            <input type="text" name="firstName" value="${personToEdit.firstName}">
+		</fieldset>
+		<fieldset>
+            <label>Job title</label>
+            <input type="text" name="jobTitle" value="${personToEdit.jobTitle}">
+		</fieldset>
+		<fieldset>
+            <label>Job area</label>
+            <input type="text" name="jobArea" value="${personToEdit.jobArea}">
+		</fieldset>
+		<fieldset>
+            <label>Phone number</label>
+            <input type="tel" name="phoneNumber" value="${personToEdit.phone}">
+		</fieldset>
+		<fieldset>
+			<button type="submit">Save</button>
+			<button type="button" class="cancel" name="cancel">Cancel</button>
+		</fieldset>
+		</div>
+		`);
+
+		 //listen for the submit event on the formPopup
+		 formPopup.addEventListener('submit', e => {
+			e.preventDefault();
+			resolve();
+			//edit the mama array
+			personToEdit.lastName = formPopup.lastName.value;
+			personToEdit.firstName = formPopup.firstName.value;
+			personToEdit.jobTitle = formPopup.jobTitle.value;
+			personToEdit.jobArea = formPopup.jobArea.value;
+			personToEdit.phone = formPopup.phoneNumber.value
+			displayList(persons)
+			console.log(personToEdit);
+            destroyPopup(formPopup);
+		}, {once: true});
+		
+		//listen for the cancel button
+		if(formPopup.cancel) {
+			formPopup.cancel.addEventListener('click', function() {
+				resolve(null);
+				destroyPopup(formPopup);
+			}, 
+			{once: true});
+		}
+
+
+		resolve(document.body.appendChild(formPopup));
+		formPopup.classList.add('open');
+	});
+
 };
 
-const deletePartner = () => {
+const deletePartner = (id) => {
 	// code delete function gere
+	let personToDelete = persons.find(person => person.id === id);
+	deletePartnerPopup(personToDelete);
 };
 
-const deleteDeletePopup = () => {
+const deletePartnerPopup = (person) => {
 	// create confirmation popup here
+	return new Promise(async function(resolve) {
+		const deletePopup = document.createElement('div');
+		deletePopup.classList.add('popup');
+        deletePopup.insertAdjacentHTML("afterbegin", `
+		
+		<div>
+			<p>Are you sure to delete <bold>${person.lastName} ${person.firstName}</bold>?</p>
+			<button class="yes">Yes</button>
+			<button class="cancel">Cancel</button>
+		</div>
+		`);
+
+		deletePopup.addEventListener('click', (e) => {
+			if(e.target.matches('button.yes')) {
+				persons = persons.filter(partner => partner.id !== person.id);
+				displayList(persons);
+				destroyPopup(deletePopup);
+				console.log(persons)
+			}
+
+			if(e.target.matches('button.cancel')){
+				destroyPopup(deletePopup);
+			}
+		})
+		resolve();
+		document.body.appendChild(deletePopup)
+		deletePopup.classList.add('open');
+	});
 };
 
 displayList(persons);
+
+// Event listeners
+document.addEventListener('click', handleClicks);
+
